7098e9d137541d2b9fbdb3973b2cf46145665109
Josh Campoverde <campo@eero.com>
2017-01-18 13:17:54 -0800

    whitespace changes to make review easier

--- src/main/scala/SemanticVersion.scala
+++ src/main/scala/SemanticVersion.scala
@@ -1,188 +1,188 @@
-package compliant
+  package compliant
 
-import scala.util.matching.Regex
-import scala.collection.mutable.ListBuffer
+  import scala.util.matching.Regex
+  import scala.collection.mutable.ListBuffer
 
-object SemanticVersion {
+  object SemanticVersion {
 
-  val re: Regex = """(\d+)\.(\d+)\.(\d+)(-([^+]+))?(\+(.+))?""".r
+    val re: Regex = """(\d+)\.(\d+)\.(\d+)(-([^+]+))?(\+(.+))?""".r
 
-  def apply(versionString: String): SemanticVersion = {
-    new SemanticVersion(versionString)
-  }
-}
-
-object Version {
-  def apply(versionString: String): SemanticVersion = {
-    new SemanticVersion(versionString)
-  }
-}
-
-class SemanticVersion(val versionString: String) extends Ordered[SemanticVersion] {
-
-  val fullVersionString: String = versionString.trim.replaceAll("\\s", "")
-
-  val (majorVersion, minorVersion, patchVersion, prerelease, buildMetadata) = {
-    fullVersionString match {
-      case SemanticVersion.re(major, minor, patch, _, pre, _, meta) =>
-        (major, minor, patch, Option(pre).getOrElse(""), Option(meta).getOrElse(""))
-      case _ =>
-        throw new IllegalArgumentException(versionString)
+    def apply(versionString: String): SemanticVersion = {
+      new SemanticVersion(versionString)
     }
   }
 
-  def versionStringWithoutBuildMetadata : String = {
-    this.fullVersionString.split("\\+").head
-  }
-
-  def ==(otherVersionString: String) : Boolean = {
-    val otherVersion = new SemanticVersion(otherVersionString)
-    this == otherVersion
-  }
-
-  def ==(otherVersion: SemanticVersion) : Boolean = {
-    this.versionStringWithoutBuildMetadata == otherVersion.versionStringWithoutBuildMetadata
-  }
-
-  def compare(that: SemanticVersion) = {
-    val majorVersionDiff = this.majorVersion.toInt - that.majorVersion.toInt
-    val minorVersionDiff = this.minorVersion.toInt - that.minorVersion.toInt
-    val patchVersionDiff = this.patchVersion.toInt - that.patchVersion.toInt
-    val prereleaseVersionDiff = this.prereleaseCompare(that)
-
-    if (this.versionStringWithoutBuildMetadata == that.versionStringWithoutBuildMetadata) {
-      0
-    } else if (majorVersionDiff != 0) {
-      majorVersionDiff
-    } else if (minorVersionDiff != 0) {
-      minorVersionDiff
-    } else if (patchVersionDiff != 0) {
-      patchVersionDiff
-    } else {
-      prereleaseVersionDiff
+  object Version {
+    def apply(versionString: String): SemanticVersion = {
+      new SemanticVersion(versionString)
     }
   }
 
-  def prereleaseCompare(that: SemanticVersion): Int = {
-    if (this.prerelease.isEmpty && !that.prerelease.isEmpty) {
-      return 1
-    } else if (!this.prerelease.isEmpty && that.prerelease.isEmpty) {
-      return -1
-    } else {
-      val thisArray = Chunkify.chunkify(this.prerelease)
-      val thatArray = Chunkify.chunkify(that.prerelease)
-      var currentDiff = 0
-      var currentIndex = 0
+  class SemanticVersion(val versionString: String) extends Ordered[SemanticVersion] {
 
-      while (currentDiff == 0 && currentIndex < thisArray.length) {
-        val currentThis = thisArray(currentIndex)
-        val currentThat = thatArray(currentIndex)
+    val fullVersionString: String = versionString.trim.replaceAll("\\s", "")
 
-        if (Chunkify.isAllDigits(currentThis) && Chunkify.isAllDigits(currentThat)) {
-          currentDiff = currentThis.toInt.compare(currentThat.toInt)
-        } else {
-          currentDiff = currentThis.compare(currentThat)
-        }
-
-        currentIndex = currentIndex + 1
+    val (majorVersion, minorVersion, patchVersion, prerelease, buildMetadata) = {
+      fullVersionString match {
+        case SemanticVersion.re(major, minor, patch, _, pre, _, meta) =>
+          (major, minor, patch, Option(pre).getOrElse(""), Option(meta).getOrElse(""))
+        case _ =>
+          throw new IllegalArgumentException(versionString)
       }
+    }
 
-      // TODO: Find a way to not use return statements
-      if (currentDiff == 0) {
-        if (thisArray.length == thatArray.length) {
-          return 0
-        } else if (thisArray.length < thatArray.length) {
-          return 1
-        } else {
-          return -1
-        }
+    def versionStringWithoutBuildMetadata : String = {
+      this.fullVersionString.split("\\+").head
+    }
+
+    def ==(otherVersionString: String) : Boolean = {
+      val otherVersion = new SemanticVersion(otherVersionString)
+      this == otherVersion
+    }
+
+    def ==(otherVersion: SemanticVersion) : Boolean = {
+      this.versionStringWithoutBuildMetadata == otherVersion.versionStringWithoutBuildMetadata
+    }
+
+    def compare(that: SemanticVersion) = {
+      val majorVersionDiff = this.majorVersion.toInt - that.majorVersion.toInt
+      val minorVersionDiff = this.minorVersion.toInt - that.minorVersion.toInt
+      val patchVersionDiff = this.patchVersion.toInt - that.patchVersion.toInt
+      val prereleaseVersionDiff = this.prereleaseCompare(that)
+
+      if (this.versionStringWithoutBuildMetadata == that.versionStringWithoutBuildMetadata) {
+        0
+      } else if (majorVersionDiff != 0) {
+        majorVersionDiff
+      } else if (minorVersionDiff != 0) {
+        minorVersionDiff
+      } else if (patchVersionDiff != 0) {
+        patchVersionDiff
       } else {
-        return currentDiff
+        prereleaseVersionDiff
       }
     }
-  }
 
-}
+    def prereleaseCompare(that: SemanticVersion): Int = {
+      if (this.prerelease.isEmpty && !that.prerelease.isEmpty) {
+        return 1
+      } else if (!this.prerelease.isEmpty && that.prerelease.isEmpty) {
+        return -1
+      } else {
+        val thisArray = Chunkify.chunkify(this.prerelease)
+        val thatArray = Chunkify.chunkify(that.prerelease)
+        var currentDiff = 0
+        var currentIndex = 0
 
-// Helper functions for chunking prerelease strings
-// TODO: Consider moving this to another file
-object Chunkify {
+        while (currentDiff == 0 && currentIndex < thisArray.length) {
+          val currentThis = thisArray(currentIndex)
+          val currentThat = thatArray(currentIndex)
 
-  def chunkify(string: String) = {
-    var chunks = new ListBuffer[String]()
-    var currentChunk = new StringBuilder
-    var currentChunkType = ""
-    var i = 0
+          if (Chunkify.isAllDigits(currentThis) && Chunkify.isAllDigits(currentThat)) {
+            currentDiff = currentThis.toInt.compare(currentThat.toInt)
+          } else {
+            currentDiff = currentThis.compare(currentThat)
+          }
 
-    while (i < string.length) {
-      var c = string(i)
+          currentIndex = currentIndex + 1
+        }
 
-      val thisChunkType = {
-        if (isDigit(c.toString)) {
-          "digit"
-        } else if (isAlpha(c.toString)) {
-          "alpha"
-        } else if (isPunctuation(c.toString)) {
-          "punctuation"
+        // TODO: Find a way to not use return statements
+        if (currentDiff == 0) {
+          if (thisArray.length == thatArray.length) {
+            return 0
+          } else if (thisArray.length < thatArray.length) {
+            return 1
+          } else {
+            return -1
+          }
         } else {
-          "unknown"
+          return currentDiff
         }
       }
+    }
 
-      // Beginning of the string or beginning of a new chunk
-      if (currentChunkType == "" || currentChunkType == thisChunkType) {
-        currentChunk += c
-        currentChunkType = thisChunkType
+  }
+
+  // Helper functions for chunking prerelease strings
+  // TODO: Consider moving this to another file
+  object Chunkify {
+
+    def chunkify(string: String) = {
+      var chunks = new ListBuffer[String]()
+      var currentChunk = new StringBuilder
+      var currentChunkType = ""
+      var i = 0
+
+      while (i < string.length) {
+        var c = string(i)
+
+        val thisChunkType = {
+          if (isDigit(c.toString)) {
+            "digit"
+          } else if (isAlpha(c.toString)) {
+            "alpha"
+          } else if (isPunctuation(c.toString)) {
+            "punctuation"
+          } else {
+            "unknown"
+          }
+        }
+
+        // Beginning of the string or beginning of a new chunk
+        if (currentChunkType == "" || currentChunkType == thisChunkType) {
+          currentChunk += c
+          currentChunkType = thisChunkType
+        } else {
+          chunks += currentChunk.toString
+          currentChunk = new StringBuilder
+          currentChunk += c
+          currentChunkType = thisChunkType
+        }
+
+        // Make sure to append the final chunk when
+        // we reach the end of the string
+        if (i == string.length - 1) {
+          chunks += currentChunk.toString
+        }
+
+        i += 1
+      }  // end string.map
+
+      chunks.toList
+    }
+
+    def isAllDigits(s: String) = {
+      s.length == s.filter(_.isDigit).length
+    }
+
+    def isDigit(s: String) = {
+      if (Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9").contains(s)) {
+        true
       } else {
-        chunks += currentChunk.toString
-        currentChunk = new StringBuilder
-        currentChunk += c
-        currentChunkType = thisChunkType
+        false
       }
+    }
 
-      // Make sure to append the final chunk when
-      // we reach the end of the string
-      if (i == string.length - 1) {
-        chunks += currentChunk.toString
+    def isPunctuation(s: String) = {
+      if (Array(".", "-", "+").contains(s)) {
+        true
+      } else {
+        false
       }
-
-      i += 1
-    }  // end string.map
-
-    chunks.toList
-  }
-
-  def isAllDigits(s: String) = {
-    s.length == s.filter(_.isDigit).length
-  }
-
-  def isDigit(s: String) = {
-    if (Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9").contains(s)) {
-      true
-    } else {
-      false
     }
-  }
 
-  def isPunctuation(s: String) = {
-    if (Array(".", "-", "+").contains(s)) {
-      true
-    } else {
-      false
+    def isAlpha(s: String) = {
+      val alphaArray = Array(
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+        "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+        "y", "z")
+
+      if (alphaArray.contains(s.toLowerCase)) {
+        true
+      } else {
+        false
+      }
     }
+
   }
-
-  def isAlpha(s: String) = {
-    val alphaArray = Array(
-      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-      "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
-      "y", "z")
-
-    if (alphaArray.contains(s.toLowerCase)) {
-      true
-    } else {
-      false
-    }
-  }
-
-}
--- src/test/scala/SemanticVersionSpec.scala
+++ src/test/scala/SemanticVersionSpec.scala
@@ -1,96 +1,96 @@
-package compliant
+  package compliant
 
-import org.scalatest.prop.PropertyChecks
+  import org.scalatest.prop.PropertyChecks
 
-class SemanticVersionSpec extends CompliantSpecBase with PropertyChecks {
-  "A SemanticVersion" when {
-    "valid" should {
-      "contain a major version" in {
-        val version = new SemanticVersion("1.2.3-alpha+meta")
-        assert(version.majorVersion == "1")
-      }
+  class SemanticVersionSpec extends CompliantSpecBase with PropertyChecks {
+    "A SemanticVersion" when {
+      "valid" should {
+        "contain a major version" in {
+          val version = new SemanticVersion("1.2.3-alpha+meta")
+          assert(version.majorVersion == "1")
+        }
 
-      "contain a minor version" in {
-        val version = new SemanticVersion("1.2.3-alpha+meta")
-        assert(version.minorVersion == "2")
-      }
+        "contain a minor version" in {
+          val version = new SemanticVersion("1.2.3-alpha+meta")
+          assert(version.minorVersion == "2")
+        }
 
-      "contain a patch version" in {
-        val version = new SemanticVersion("1.2.3-alpha+meta")
-        assert(version.patchVersion == "3")
-      }
+        "contain a patch version" in {
+          val version = new SemanticVersion("1.2.3-alpha+meta")
+          assert(version.patchVersion == "3")
+        }
 
-      "contain a prerelease attribute" in {
-        val version = new SemanticVersion("1.2.3-alpha+meta")
-        assert(version.prerelease == "alpha")
-      }
+        "contain a prerelease attribute" in {
+          val version = new SemanticVersion("1.2.3-alpha+meta")
+          assert(version.prerelease == "alpha")
+        }
 
-      "contain build metadata" in {
-        val version = new SemanticVersion("1.2.3-alpha+meta")
-        assert(version.buildMetadata == "meta")
-      }
-    }
-
-    "evaluting equality of SemanticVersions" should {
-      "return true for exactly equal versions" in {
-        val firstVersion = new SemanticVersion("1.0.0-alpha+more")
-        val secondVersion = new SemanticVersion("1.0.0-alpha+more")
-        val result = firstVersion == secondVersion
-        assert(result == true)
-      }
-
-      "return true for equal versions with different metadata" in {
-        val firstVersion = new SemanticVersion("1.0.0-alpha+more")
-        val secondVersion = new SemanticVersion("1.0.0-alpha+different")
-        val result = firstVersion == secondVersion
-        assert(result == true)
-      }
-    }
-
-    "comparing versions" should {
-
-      val equal = Table[String, String](
-        ("a", "b"),
-        ("1.0.0", "1.0.0"),
-        ("0.1.0", "0.1.0"),
-        ("0.0.1", "0.0.1"),
-        ("1.0.0+meta1", "1.0.0+meta2")  // build metadata disregarded for ordering
-      )
-
-      "properly identify equal versions as equal" in {
-        forAll(equal) { (a, b) =>
-          val versionA = new SemanticVersion(a)
-          val versionB = new SemanticVersion(b)
-          assert(versionA == versionB)
+        "contain build metadata" in {
+          val version = new SemanticVersion("1.2.3-alpha+meta")
+          assert(version.buildMetadata == "meta")
         }
       }
 
-      val inequal = Table[String, String](
-        ("lesser", "greater"),
-        ("1.0.0", "2.0.0"),
-        ("1.0.0", "10.0.0"),
-        ("2.0.0", "10.0.0"),
-        ("0.1.0", "0.2.0"),
-        ("0.1.0", "0.10.0"),
-        ("0.2.0", "0.10.0"),
-        ("0.0.1", "0.0.2"),
-        ("0.0.1", "0.0.10"),
-        ("0.0.2", "0.0.10"),
-        ("1.0.0-alpha", "1.0.0-beta"),   // pre-release versions
-        ("1.1.0-20160605", "1.1.0-alpha"),  // numeric identifiers have lower precedence
-        ("1.0.0-20170109", "1.0.0-20170110"),
-        ("1.0.0-2", "1.0.0-10")
-      )
+      "evaluting equality of SemanticVersions" should {
+        "return true for exactly equal versions" in {
+          val firstVersion = new SemanticVersion("1.0.0-alpha+more")
+          val secondVersion = new SemanticVersion("1.0.0-alpha+more")
+          val result = firstVersion == secondVersion
+          assert(result == true)
+        }
 
-      "properly determine ordering for inequal versions" in {
-        forAll(inequal) { (lesser, greater) =>
-          val lesserVersion = new SemanticVersion(lesser)
-          val greaterVersion = new SemanticVersion(greater)
-          assert(lesserVersion < greaterVersion)
-          assert(greaterVersion > lesserVersion)
+        "return true for equal versions with different metadata" in {
+          val firstVersion = new SemanticVersion("1.0.0-alpha+more")
+          val secondVersion = new SemanticVersion("1.0.0-alpha+different")
+          val result = firstVersion == secondVersion
+          assert(result == true)
         }
       }
 
+      "comparing versions" should {
+
+        val equal = Table[String, String](
+          ("a", "b"),
+          ("1.0.0", "1.0.0"),
+          ("0.1.0", "0.1.0"),
+          ("0.0.1", "0.0.1"),
+          ("1.0.0+meta1", "1.0.0+meta2")  // build metadata disregarded for ordering
+        )
+
+        "properly identify equal versions as equal" in {
+          forAll(equal) { (a, b) =>
+            val versionA = new SemanticVersion(a)
+            val versionB = new SemanticVersion(b)
+            assert(versionA == versionB)
+          }
+        }
+
+        val inequal = Table[String, String](
+          ("lesser", "greater"),
+          ("1.0.0", "2.0.0"),
+          ("1.0.0", "10.0.0"),
+          ("2.0.0", "10.0.0"),
+          ("0.1.0", "0.2.0"),
+          ("0.1.0", "0.10.0"),
+          ("0.2.0", "0.10.0"),
+          ("0.0.1", "0.0.2"),
+          ("0.0.1", "0.0.10"),
+          ("0.0.2", "0.0.10"),
+          ("1.0.0-alpha", "1.0.0-beta"),   // pre-release versions
+          ("1.1.0-20160605", "1.1.0-alpha"),  // numeric identifiers have lower precedence
+          ("1.0.0-20170109", "1.0.0-20170110"),
+          ("1.0.0-2", "1.0.0-10")
+        )
+
+        "properly determine ordering for inequal versions" in {
+          forAll(inequal) { (lesser, greater) =>
+            val lesserVersion = new SemanticVersion(lesser)
+            val greaterVersion = new SemanticVersion(greater)
+            assert(lesserVersion < greaterVersion)
+            assert(greaterVersion > lesserVersion)
+          }
+        }
+
+      }
     }
   }
-}
